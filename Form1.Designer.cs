459e6c6667cc5411926065ef370b40da314cfd20
ryh <renyh@dp2003.com>
2021-06-07 13:45:27 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace test_xpath
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_xml = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_xpath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "xml";
+            // 
+            // textBox_xml
+            // 
+            this.textBox_xml.Location = new System.Drawing.Point(72, 24);
+            this.textBox_xml.Multiline = true;
+            this.textBox_xml.Name = "textBox_xml";
+            this.textBox_xml.Size = new System.Drawing.Size(669, 231);
+            this.textBox_xml.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "xpath";
+            // 
+            // textBox_xpath
+            // 
+            this.textBox_xpath.Location = new System.Drawing.Point(71, 294);
+            this.textBox_xpath.Name = "textBox_xpath";
+            this.textBox_xpath.Size = new System.Drawing.Size(669, 28);
+            this.textBox_xpath.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "查找-只返回第一个节点";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "查找-返回全部节点";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_xpath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_xml);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "测试Xpath小工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_xml;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_xpath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using DigitalPlatform.Xml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace test_xpath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string xml = this.textBox_xml.Text.Trim();
+            string xpath = this.textBox_xpath.Text.Trim();
+            XmlDocument dom = new XmlDocument();
+            dom.LoadXml(xml);
+
+            XmlNode rootNode = dom.DocumentElement;
+            string outerXml = DomUtil.GetElementOuterXml(rootNode, xpath);
+            string innerXml = DomUtil.GetElementInnerXml(rootNode, xpath);
+            string text = DomUtil.GetElementInnerText(rootNode, xpath);
+
+            string info = "outerXml:" + outerXml + "\r\n"
+                + "innerXml:" + innerXml + "\r\n"
+                + "text:" + text;
+
+            MessageBox.Show(this, info);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string xml = this.textBox_xml.Text.Trim();
+            string xpath = this.textBox_xpath.Text.Trim();
+            XmlDocument dom = new XmlDocument();
+            dom.LoadXml(xml);
+
+            XmlNode rootNode = dom.DocumentElement;
+
+            XmlNodeList list = rootNode.SelectNodes(xpath);
+            string info = "共找到["+list.Count+"]个节点"+"\r\n";
+            foreach (XmlNode node in list)
+            {
+                info += node.InnerText + "\r\n";
+            
+            }
+
+            MessageBox.Show(this, info);
+
+        }
+    }
+}
